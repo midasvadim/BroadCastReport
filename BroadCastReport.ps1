f28b188b3c505f80cc152fec9a764f955d163323
Smal, Vadim <vadim.smal@gmail.com>
2016-02-01 12:29:33 -0500

    Add Popup done

--- BroadCastReport.ps1
+++ BroadCastReport.ps1
@@ -144,7 +144,8 @@ function init
 
 	}
 	
-	
+	$wshell = New-Object -ComObject Wscript.Shell	
+	$wshell.Popup("Operation Completed", 0, "Done", 0x1)
 }
 
 ## Find appropriate Div folder to move from 
